bed258474fd2f00f441c1f997e5db804a0e93aeb
tim-hub <tim_w@outlook.com>
2020-03-21 12:17:16 +1300

    remove knew and db

--- src/services/transaction/controller.ts
+++ src/services/transaction/controller.ts
@@ -1,11 +1,8 @@
-import { getKnexDB } from "../../utils/db";
+
 import path from "path";
 
 const sampleDB= path.resolve(__dirname, '../../../demo.mmb');
 
 export const index = async () => {
-    const client = getKnexDB(sampleDB);
-    const r= await client.select('*').from('CHECKINGACCOUNT_V1');
-    client.destroy();
-    return r;
+    return {}
 };
