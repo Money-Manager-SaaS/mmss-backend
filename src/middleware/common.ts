838094f858fb0d4c2d9443bb66b3292beabd9b92
tim-hub <tim_w@outlook.com>
2020-04-04 16:25:24 +1300

    add comments for CORS config

--- src/middleware/common.ts
+++ src/middleware/common.ts
@@ -3,8 +3,15 @@ import cors from "cors";
 import parser from "body-parser";
 import compression from "compression";
 
+const corsOptions = {
+    origin: 'http://github.io',
+    // origin: 'http://github.io', //
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 export const handleCors = (router: Router) =>
-    router.use(cors({credentials: true, origin: true}));
+    router.use(cors({credentials: true, origin: true})); // allow all
+    // router.use(cors(corsOptions)); // allow the whitelist domain name only
 
 export const handleBodyRequestParsing = (router: Router) => {
     router.use(parser.urlencoded({extended: true}));
