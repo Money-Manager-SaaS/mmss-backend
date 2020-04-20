57b52f74e9bbc0e842283b8dda66689edc249f18
girl817 <jennifer.jyjiang@gmail.com>
2020-04-20 21:34:32 +1200

    add seeder for user model

--- src/db/seeder.ts
+++ src/db/seeder.ts
@@ -1,6 +1,7 @@
 import Account from './models/Account';
 import Transaction from './models/Transaction';
 import Category from './models/Category';
+import User from './models/User';
 
 
 export const init = async () => {
@@ -8,6 +9,7 @@ export const init = async () => {
   await Account.sync({force: forceSync});
   await Transaction.sync({force: forceSync});
   await Category.sync({force: forceSync});
+  await User.sync({force: forceSync});
 
   const cash = await Account.create({
     name: 'cash',
@@ -56,6 +58,23 @@ export const init = async () => {
     accountID: bank.id,
     note: 'an income'
   });
+
+  const jack = await User.create({
+    username: 'jack',
+    email: 'jack.JJJ@gmail.com',
+    last_login: '2010/08/19',
+    date_login: '2011/09/10',
+    passwordhash: '89djheddkhdee33'
+  });
+
+  const tom = await User.create({
+    username: 'tom',
+    email: 'tom.c@Kmail.com',
+    last_login: '2011/10/19',
+    date_login: '2018/03/18',
+    passwordhash: '90d0dke888dje3j'
+  });
+
   console.log('db seeding done');
 };
 
