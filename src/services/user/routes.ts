72b1cb61bd2cb196dda02536f332f9be2e6428c1
girl817 <jennifer.jyjiang@gmail.com>
2020-04-20 21:15:27 +1200

    update user service routes and index.

--- src/services/user/routes.ts
+++ src/services/user/routes.ts
@@ -3,27 +3,27 @@ import * as services from './service';
 
 export default [
     {
-        path: "/api/v1/transactions",
+        path: "/api/v1/users",
         method: "get",
         handler: [services.getAll]
     },
     {
-        path: "/api/v1/transactions/:id",
+        path: "/api/v1/users/:id",
         method: "get",
         handler: [services.getOne]
     },
     {
-        path: "/api/v1/transactions",
+        path: "/api/v1/users",
         method: "post",
         handler: [services.create]
     },
     {
-        path: "/api/v1/transactions/:id",
+        path: "/api/v1/users/:id",
         method: "put",
         handler: [services.updateOne]
     },
     {
-        path: "/api/v1/transactions/:id",
+        path: "/api/v1/users/:id",
         method: "delete",
         handler: [services.deleteOne]
     }
