8aeb8211bdc84dc8093ba8ec2e706108afd3eee4
girl817 <jennifer.jyjiang@gmail.com>
2020-05-03 19:03:33 +1200

    add ledger service

---
+++ src/services/ledger/provider.ts
@@ -0,0 +1,23 @@
+import Account from '../../db/models/Ledger';
+
+export const getAll = async () => {
+    const trans = await Ledgers.findAll();
+    return {ledgers: trans};
+};
+
+export const getOne = async (id: number) => {
+    const tran = await Ledger.findByPk(id);
+    return tran;
+};
+
+export const updateOne = async (id: number, category: ledger) => {
+    const t: Ledger | null = await Ledger.findByPk(id);
+    !!t && await t.update(category);
+    return t;
+};
+
+export const deleteOne = async (id: number) => {
+    const t: Ledger | null = await Ledger.findByPk(id);
+    !!t && await t.destroy();
+    return t;
+};
