1c17f47a7acd385e4a4f3759d02a20f374f7a576
donchan <chendong.jy@gmail.com>
2020-06-09 21:16:20 +1200

    add testing case for signIn and signUp provider.

--- src/services/user/provider.test.ts
+++ src/services/user/provider.test.ts
@@ -1,7 +1,7 @@
 import {signUp, signIn, encryptPassword, validate} from './provider';
+import User from '../../db/models/User';
 
 describe('encryptPassword', ()=> {
-
   it('encryptPassword', ()=> {
     expect(encryptPassword("12345")).toEqual("158ee933a64b8629913bbb87046a0336a615e086");
   })
@@ -9,6 +9,33 @@ describe('encryptPassword', ()=> {
   it('validate', ()=> {
     expect(validate("12345")).toEqual("Password must be at least 6 characters");
   })
-
 });
+
+
+describe('signUp', ()=> {
+  it('signUp', async ()=> {
+    signUp({
+      userName: "tim",
+      email: "tim@gmail.com",
+      password: encryptPassword("123456"),
+  });
+
+  const user = await User.findOne({
+    where: {
+        userName: "tim",
+    },
+  });
+  console.log("user!.id", user!.id);  
+  expect(user!.id).toBeGreaterThan(0);
+  })
+});
+
+describe('signIn', ()=> {
+  it('signIn', async ()=> {
+    const [token, id, email] = await signIn("tim", "123456");
+    expect(email).toEqual("tim@gmail.com");
+  })
+}
+);
+
 
