fd606b877afe22420c63253837e049089908a702
donchan <chendong.jy@gmail.com>
2020-06-09 21:01:19 +1200

    add a silly testing case.

--- src/services/user/provider.test.ts
+++ src/services/user/provider.test.ts
@@ -3,8 +3,12 @@ import {signUp, signIn, encryptPassword, validate} from './provider';
 describe('encryptPassword', ()=> {
 
   it('encryptPassword', ()=> {
-    encryptPassword("12345");
     expect(encryptPassword("12345")).toEqual("158ee933a64b8629913bbb87046a0336a615e086");
   })
+
+  it('validate', ()=> {
+    expect(validate("12345")).toEqual("Password must be at least 6 characters");
+  })
+
 });
 
