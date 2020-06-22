b7478c1eca3f7e305be176903d881f79ae0b9cce
donchan <chendong.jy@gmail.com>
2020-06-22 18:00:54 +1200

    no auth on signin and signup request.

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction, Router } from "express";
 
 export const checkAuth = (router: Router) => {
     router.use((req: Request, res: Response, next: NextFunction) => {
-        if (!req.cookies._id) {
+        if (req.path.indexOf("sigin") != -1 && req.path.indexOf("signup") != -1 && !req.cookies._id) {
             throw new HTTP400Error("Please login first!");
         } else {
             next();
