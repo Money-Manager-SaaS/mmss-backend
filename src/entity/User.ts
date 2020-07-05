fbc0704d8b32a2e8a8391be3e4eb7870ff0e3a67
tim-hub <tim_w@outlook.com>
2020-07-05 15:43:47 +1200

    update user model

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,54 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity} from "typeorm";
 
 @Entity()
-export class User {
+export class User extends BaseEntity{
 
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
-    firstName: string;
+    @Column({
+        length: 128,
+    })
+    email: string;
 
-    @Column()
-    lastName: string;
+    @Column({
+        length: 128,
+    })
+    password: string;
 
-    @Column()
-    age: number;
+    @Column({
+        length: 64,
+    })
+    userName: string;
+
+    @Column({
+        type: 'boolean'
+    })
+    active: boolean;
+
+    @Column({
+        type: 'datetime'
+    })
+    lastLogin: Date;
+
+    @Column({
+        type: 'text'
+    })
+    about?: string;
+
+    @Column({
+        type: 'int'
+    })
+    age?: number;
+
+    @Column({
+        length: 64,
+    })
+    firstName?: string;
+
+    @Column({
+        length: 64,
+    })
+    lastName?: string;
 
 }
