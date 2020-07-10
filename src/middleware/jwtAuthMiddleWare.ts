25fc0b18b7514bc29d36a39d95120cb7fc0e869a
tim-hub <tim_w@outlook.com>
2020-07-10 20:38:04 +1200

    fix bypassing access token

--- src/middleware/jwtAuthMiddleWare.ts
+++ src/middleware/jwtAuthMiddleWare.ts
@@ -1,4 +1,5 @@
 import { verifyAccessToken } from '../services/auth/provider';
+import { User } from '../entity/User';
 
 /**
  *  the authorization header is required in request
@@ -7,26 +8,33 @@ import { verifyAccessToken } from '../services/auth/provider';
  * @param next
  */
 export const authenticateJWT = async (req, res, next) => {
-  if (process.env.BYPATH_ACCESS_TOKEN && process.env.NODE_ENV === 'development' && !process.env.JEST_TESTING) {
+  if (process.env.BYPATH_ACCESS_TOKEN === 'true' && process.env.NODE_ENV === 'development' && process.env.JEST_TESTING !== 'true') {
     // for local development only
-    next()
-  }
-
-  const authHeader = req.headers.authorization;
-  if (authHeader) {
-    const token = authHeader;
-    try {
-      const payload = await verifyAccessToken(token);
-      if (payload.exp <= new Date().getTime()) {
+    console.log('bypassing the access token');
+    const user = (await User.getRepo().find())[0];
+    req.user = user;
+    req.userID = user.id;
+    req.email = user.email;
+    next();
+  } else {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader;
+      try {
+        const payload = await verifyAccessToken(token);
+        if (payload.exp <= new Date().getTime()) {
+          res.sendStatus(401);
+        }
+        const user = (await User.getRepo().findOne(payload.userID))[0];
+        req.user = user;
+        req.userID = payload.userID;
+        req.email = payload.email;
+        next();
+      } catch (e) {
         res.sendStatus(401);
       }
-      req.userID = payload.userID;
-      req.email = payload.email;
-      next();
-    } catch (e) {
+    } else {
       res.sendStatus(401);
     }
-  } else {
-    res.sendStatus(401);
   }
 };
