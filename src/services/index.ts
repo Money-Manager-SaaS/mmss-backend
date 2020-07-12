e74028f3c35ef2f79386f7d66dd5c22f9cfccc94
tim-hub <tim_w@outlook.com>
2020-07-12 17:06:09 +1200

    add payee part

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,12 +2,14 @@ import * as express from 'express';
 import ledgerRouter from './ledger/ledgerRouter';
 import accountRouter from './account/accountRouter';
 import categoryRouter from './category/categoryRouter';
+import payeeRouter from './payee/payeeRouter';
 
 const router = express.Router();
-// middlewares are used on each router seperately
+// middleware are used on each router seperately
 
 router.use('/ledgers', ledgerRouter);
 router.use('/accounts', accountRouter);
 router.use('/categories', categoryRouter);
+router.use('/payees', payeeRouter);
 
 export default router;
